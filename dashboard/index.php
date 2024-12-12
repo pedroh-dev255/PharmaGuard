da1417a9eb3072f89430e79e287e8d738cf86c5d
Pedro Henrique Santos Souza <pedro.santos@palmed.local>
2024-12-12 17:35:13 -0300

    configuração inicial para uso do .env para segurança

--- dashboard/index.php
+++ dashboard/index.php
@@ -122,7 +122,7 @@
     <link rel="stylesheet" href="../css/dash.css">
     <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <title>Dashboard</title>
+    <title>PharmaGuard</title>
     <style>
         /* Estilo para a caixa modal */
         .modal {
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
-    <title>Login</title>
+    <title>PharmaGuard</title>
     <link rel="stylesheet" href="css/css.css">
 </head>
 <body>
@@ -13,8 +13,8 @@
         <form method="POST" action="routes/formLogin.php" class="formLogin">
             <h1>Login</h1>
             <p>Digite os seus dados de acesso no campo abaixo.</p>
-            <label for="user">Usuario</label>
-            <input type="text" placeholder="Digite seu usuario" name="user" autofocus="true" required/>
+            <label for="user">Email</label>
+            <input type="email" placeholder="Digite seu email" name="user" autofocus="true" required/>
             <label for="password">Senha</label>
             <input type="password" placeholder="Digite sua senha" name="senha" minlength="6" required/>
             <?php 
